34933c4edf129ef4b4ad500c9d0e01c309218216
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-06-18 18:16:48 +0600

    feat: pre-work

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header"></header>
+      <main className="main"></main>
+      <footer className="footer"></footer>
+    </div>
+  );
+}
+
+export default App;
